7c1fec3a38378759eb2e76cf8b6c36733d96bf1b
Brian H. Ward <glyphrider@gmail.com>
2020-11-23 12:58:43 -0500

    data driven

--- src/main/java/org/glyphrider/roman/Roman.java
+++ src/main/java/org/glyphrider/roman/Roman.java
@@ -1,19 +1,32 @@
 package org.glyphrider.roman;
 
 public class Roman {
+    private class RomanConversionDataItem {
+	public int arabic;
+	public String roman;
+	public RomanConversionDataItem(int arabic, String roman) {
+	    this.arabic = arabic;
+	    this.roman = roman;
+	}
+    }
+
+    private RomanConversionDataItem[] conversionData = {
+	new RomanConversionDataItem(10,"X"),
+	new RomanConversionDataItem(9,"IX"),
+	new RomanConversionDataItem(5,"V"),
+	new RomanConversionDataItem(4,"IV"),
+	new RomanConversionDataItem(1,"I")
+    };
+						     
     public String to(int arabic) {
         String result;
         result = "";
-        while(arabic >= 5) {
-            result += "V";
-            arabic -= 5;
-        }
-        while(arabic >= 4) {
-            result += "IV";
-            arabic -= 4;
-        }
-        while(arabic-- > 0)
-            result += "I";
+	for(RomanConversionDataItem item : conversionData) {
+	    while(arabic >= item.arabic) {
+		result += item.roman;
+		arabic -= item.arabic;
+	    }
+	}
         return result;
     }
 }
